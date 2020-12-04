171e6a6592f4e26241f03e103649affd1ec22830
brinxade <57074626+brinxade@users.noreply.github.com>
2020-12-04 02:20:34 -0500

    bug fixes

--- watch.php
+++ watch.php
@@ -10,31 +10,33 @@
 	<?php echo $commons["page_preloader"]; ?>
 	<header id="header"></header>
 
-	<main id="watch-movie">
-		<div id="movie-panel">
-			<video id="movie" controls src="http://media.w3.org/2010/05/sintel/trailer.mp4"></video>
-			<div class="inner">
-				<h4 class="title">Comments</h4>
-				<div id="comments">
-					<p>No comments for this movie yet</p>
+	<main class="base-padding">
+		<div id="watch-movie">
+			<div id="movie-panel">
+				<video id="movie" controls src="http://media.w3.org/2010/05/sintel/trailer.mp4"></video>
+				<div class="inner">
+					<h4 class="title">Comments</h4>
+					<div id="comments">
+						<p>No comments for this movie yet</p>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div id="movie-info-panel">
-			<div id="movie-image"><img src=""/></div>
-			<div class="inner">
-				<h4 id="movie-name" class="title"></h4>
-				<p id="movie-desc"></p>
-				<div id="user-comment">
-					<h5 class="title">Post a Comment</h5>
-					<div class="user-comment-inner">
-						<input type="text" placeholder="Your Name" class="name" required/>
-						<input type="email" placeholder="Your Email" class="email" required/>
-						<input type="text" placeholder="Your Comment" class="input-comment" required/>
+			<div id="movie-info-panel">
+				<div id="movie-image"><img src=""/></div>
+				<div class="inner">
+					<h4 id="movie-name" class="title"></h4>
+					<p id="movie-desc"></p>
+					<div id="user-comment">
+						<h5 class="title">Post a Comment</h5>
+						<div class="user-comment-inner">
+							<input type="text" placeholder="Your Name" class="name" required/>
+							<input type="email" placeholder="Your Email" class="email" required/>
+							<input type="text" placeholder="Your Comment" class="input-comment" required/>
+						</div>
+						<button class="button">Submit &nbsp;&nbsp;<i class="far fa-paper-plane"></i></button>
 					</div>
-					<button class="button">Submit &nbsp;&nbsp;<i class="far fa-paper-plane"></i></button>
+					<p id="comment-response"></p>
 				</div>
-				<p id="comment-response"></p>
 			</div>
 		</div>
 	</main>
